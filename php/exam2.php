e2170249e74a2daa40229563cb1aad50bf6d437b
AnirudhMishra99 <anirudhmishra37411@gmail.com>
2024-08-02 16:23:25 +0530

    php

---
+++ php/exam2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+var x=10,y=20;
+var z=x+y;
+document.write("sum of"+x+"and"+y+" is :"+z);        
+    </script>      
+</head>
+<body>
+    <?php
+    $x=10;
+    $y=20;
+    $z=$x+$y;
+    echo " <br>sum of ".$x. "and" .$y. "is :" .$z;
+    ?>
+</body>
+</html>
